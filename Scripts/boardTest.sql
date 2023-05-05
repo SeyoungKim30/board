f9816447c0c602853bddfd18b958837690a26842
sengu30 <dearseng@gmail.com>
2023-05-06 01:58:27 +0900

    연관게시물 sql 작성

--- Scripts/boardTest.sql
+++ Scripts/boardTest.sql
@@ -3,11 +3,6 @@ CREATE TABLE boardvoca (
 	voca varchar2(20)
 	);
 
-INSERT ALL 
-INTO boardvoca(postid,voca) VALUES (123,'234234')
-INTO boardvoca(postid,voca) VALUES (2,'asdf')
-SELECT * FROM dual;
-
 SELECT * FROM BOARDVOCA ;
 
 DELETE boardvoca;
@@ -19,5 +14,26 @@ SELECT count(DISTINCT voca),count(voca) FROM boardvoca ;
 
 SELECT voca FROM boardvoca WHERE postid=184;
 
---해당 단어가 있는 글의 갯수를 구하기
-SELECT * FROM 
+--특정글의 40퍼 이하인 키워드
+SELECT * 
+FROM 
+	(SELECT voca, COUNT(postid) countpost FROM boardVoca 
+	WHERE VOCA IN (SELECT voca FROM boardVoca WHERE POSTID='163') 
+	GROUP BY voca) sub1
+WHERE countpost/(SELECT COUNT(DISTINCT postID) FROM boardVoca)<=0.4
+	AND countpost>1;
+	
+--같은 키워드가 2개 이상인 글
+SELECT POSTID, COUNt(voca)
+FROM boardVoca
+WHERE voca IN (
+	SELECT voca
+	FROM 
+		(SELECT voca, COUNT(postid) countpost FROM boardVoca 
+		WHERE VOCA IN (SELECT voca FROM boardVoca WHERE POSTID='163') 
+		GROUP BY voca) sub1
+	WHERE countpost/(SELECT COUNT(DISTINCT postID) FROM boardVoca)<=0.4
+		AND countpost>1
+	)
+GROUP BY POSTID 
+;
