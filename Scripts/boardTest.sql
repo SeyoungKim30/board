f2c38d6b90f761810780330e94d268df8fc35972
sengu30 <dearseng@gmail.com>
2023-05-07 02:54:22 +0900

    complete boardVoca frequency

--- Scripts/boardTest.sql
+++ Scripts/boardTest.sql
@@ -20,46 +20,41 @@ WHERE countpost/(SELECT COUNT(DISTINCT postID) FROM boardVoca)<=0.4
 	AND countpost>1;
 	
 --같은 키워드가 2개 이상인 글
-SELECT POSTID, COUNt(voca) countvoca
+SELECT POSTID , voca, frequency
 FROM boardVoca
 WHERE voca IN (
 	SELECT voca
 	FROM 
 		(SELECT voca, COUNT(postid) countpost FROM boardVoca 
-		WHERE VOCA IN (SELECT voca FROM boardVoca WHERE POSTID='198') 
+		WHERE VOCA IN (SELECT voca FROM boardVoca WHERE POSTID='200') 
 		GROUP BY voca) sub1
 	WHERE countpost/(SELECT COUNT(DISTINCT postID) FROM boardVoca)<=0.4
 		AND countpost>1
 	)
-GROUP BY POSTID 
 ;
+--사용가능 단어 중 점수랑 갯수 구하기
+SELECT POSTID, voca,frequency FROM boardVoca WHERE voca IN (	--사용가능 단어가 포스트별로 몇개 있는지
+			SELECT voca FROM 		--이 글의 단어를 가진 글의 수/ 전체글의 수 = 0.4인 단어만 골라내기
+				(SELECT voca, COUNT(postid) countpost FROM boardVoca WHERE VOCA IN (SELECT voca FROM boardVoca WHERE POSTID='200') GROUP BY voca) sub1		--이 글의 단어를 가진 글의 수
+			WHERE countpost/(SELECT COUNT(DISTINCT postID) FROM boardVoca)<=0.4		--이 글의 단어를 가진 글의 수/ 전체글의 수 = 0.4인 단어만 골라내기
+) ;
+--글 별 점수 구하기
+SELECT postid, count(voca), sum(frequency) FROM (SELECT POSTID, voca,frequency FROM boardVoca WHERE voca IN (	--사용가능 단어가 포스트별로 몇개 있는지
+			SELECT voca FROM 		--이 글의 단어를 가진 글의 수/ 전체글의 수 = 0.4인 단어만 골라내기
+				(SELECT voca, COUNT(postid) countpost FROM boardVoca WHERE VOCA IN (SELECT voca FROM boardVoca WHERE POSTID='200') GROUP BY voca) sub1		--이 글의 단어를 가진 글의 수
+			WHERE countpost/(SELECT COUNT(DISTINCT postID) FROM boardVoca)<=0.4		--이 글의 단어를 가진 글의 수/ 전체글의 수 = 0.4인 단어만 골라내기
+))sub4 GROUP BY postid;
+
+
 --카운트보카 큰 순서대로 글 찾기
-SELECT b.postid, b.SUBJECT, b.REGDTE, countvoca
-FROM BOARD b , (SELECT POSTID, COUNt(voca) countvoca
-						FROM boardVoca
-						WHERE voca IN (
-							SELECT voca
-							FROM 
-								(SELECT voca, COUNT(postid) countpost FROM boardVoca 
-								WHERE VOCA IN (SELECT voca FROM boardVoca WHERE POSTID='200') 
-								GROUP BY voca) sub1
-							WHERE countpost/(SELECT COUNT(DISTINCT postID) FROM boardVoca)<=0.4
-							)
-						GROUP BY POSTID ) sub3
-WHERE b.POSTID = sub3.postid AND countvoca>1
-ORDER BY COUNTVOCA desc;
+SELECT b.postid, b.SUBJECT, b.REGDTE, countvoca,sumFREQUENCY
+FROM BOARD b , 
+	(SELECT POSTID, count(voca) countvoca,sum(frequency) sumFREQUENCY  FROM boardVoca WHERE voca IN (	--사용가능 단어가 포스트별로 몇개 있는지
+			SELECT voca FROM 		--이 글의 단어를 가진 글의 수/ 전체글의 수 = 0.4인 단어만 골라내기
+				(SELECT voca, COUNT(postid) countpost FROM boardVoca WHERE VOCA IN (SELECT voca FROM boardVoca WHERE POSTID='200') GROUP BY voca) sub1		--이 글의 단어를 가진 글의 수
+			WHERE countpost/(SELECT COUNT(DISTINCT postID) FROM boardVoca)<=0.4		--이 글의 단어를 가진 글의 수/ 전체글의 수 = 0.4인 단어만 골라내기
+	) GROUP BY POSTID ) sub3
+WHERE b.POSTID = sub3.postid AND countvoca>=2
+ORDER BY sumFREQUENCY desc;
 
---게시글 순서대로 안떠
-select b3.postid, b3.subject,b3.regdte, rn from 
- (SELECT b.*, rownum rn from
-	(select b2.* from board b2 
-	WHERE subject LIKE '%'||''||'%'
-	order by regdte DESC
-	) b) b3
-	where rn between 1+(30*(2-1)) and (30*2)
-order by rn
-;
 
---board 회원
-
-SELECT * FROM boardmember ;
